45df10958979240e74070696469060b7dca28d65
VivekBhawsar18 <vivekcoder18@gmail.com>
2023-01-29 20:42:46 +0530

    update

---
+++ Flaskapp/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+from Flaskapp.extensions import db
+
+app = Flask(__name__)
+
+# App configuration
+app.config.from_object(Config)
+
+# Initialize Flask extensions here
+db.init_app(app)
+
+# Register blueprints here
+from Flaskapp.test.views import bp as Test_bp
+app.register_blueprint(Test_bp ,url_prefix ='/test')
+
+from Flaskapp.home.views import bp as Home_bp
+app.register_blueprint(Home_bp , url_prefix = '/home')
+
+from Flaskapp.admin.views import bp as Admin_bp
+app.register_blueprint(Admin_bp , url_prefix = '/admin')
---
+++ Flaskapp/test/views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+from Flaskapp.models.test import Test
+
+bp = Blueprint('test' , __name__ , static_folder="static" , template_folder="templates")
+
+@bp.route('/')
+def index():
+    return "<h1>Index page of Test blueprint</h1>"
+
+@bp.route('/dbcon')
+def db_con():
+    try:
+        data = Test.query.all()
+        return render_template('dbcon.html',names = data)
+
+    except Exception as e:
+        return str(e)
+
