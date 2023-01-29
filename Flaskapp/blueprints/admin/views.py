f26077c52aef448a66493e52b25c3270822d9158
VivekBhawsar18 <vivekcoder18@gmail.com>
2023-01-30 00:38:56 +0530

    update

--- Flaskapp/blueprints/admin/views.py
+++ Flaskapp/blueprints/admin/views.py
@@ -1,14 +1,14 @@
-from flask import Blueprint , render_template , request , redirect , url_for
+from flask import Blueprint, flash , render_template , request , redirect , url_for , session
 import os
 
 bp = Blueprint('admin' , __name__ , static_folder='static' , template_folder='templates')
 
 @bp.route('/login')
 def admin_login():
-    return render_template('alogin.html')
+    return render_template('adminLogin.html')
 
 @bp.route('/home' , methods=['POST' , 'GET'])
-def home():
+def admin_home():
     if request.method == 'POST':
         username = request.form.get('username') 
         password = request.form.get('password')
@@ -17,9 +17,20 @@ def home():
         pwd  = os.environ.get('ADMIN_PASS')
 
         if ( username==name and password==pwd ):
-            return "<h1>Login success</h1>"
+            session['user'] = name
+            flash("login success","info")
+            return render_template('adminHome.html')
 
         else:
-            return "<h1>Login failed</h1>"
-    return redirect(url_for('login'))
+            flash("Invalid Credentials","danger")
+            return redirect(url_for('admin.admin_login'))
 
+    return redirect(url_for('admin.admin_login'))
+
+@bp.route("/logoutAdmin")
+def admin_logout():
+    
+    session.pop('user')
+    flash("You are logout admin", "primary")
+
+    return redirect(url_for('admin.admin_login'))
